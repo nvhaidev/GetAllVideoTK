7ba3886068955080dd27d11bb380ed95fcf87e19
Nguyễn Văn Hải <91068849+nvhaidev@users.noreply.github.com>
2021-12-29 09:48:05 +0700

    Create getLinkVideoDouyinAllToTxt.js

---
+++ getLinkVideoDouyinAllToTxt.js
@@ -0,0 +1,34 @@
+javascript: (function () {
+    var video = 0;
+
+    function pageScroll() {
+        window.scrollBy(0, 1000);
+        let max = document.querySelectorAll('li._4P45SWS7').length;
+        video += 2;
+        if (video > max) {
+            const myNodelist = document.querySelectorAll("li._4P45SWS7");
+            const uid = window.location.href.split('/')[4].split('?')[0];
+            const link = [];
+            for (let i = 0; i < myNodelist.length; i++) {
+                link.push(`https:${document.querySelectorAll("li._4P45SWS7")[i][Object.keys(document.querySelectorAll('li._4P45SWS7')[i])[0]].memoizedProps.children.props.awemeInfo.video.playApi}`)
+
+
+            }
+            const file = new Blob([JSON.stringify(link)], {type: 'text/plain'});
+            const url = window.URL.createObjectURL(file);
+            const b = document.createElement("a");
+            b.style.display = "none";
+            b.href = url;
+
+            b.setAttribute("download", `${uid}-Namdeptrai.txt`);
+            b.download = `${uid}-Namdeptrai.txt`;
+            document.body.appendChild(b);
+            b.click();
+            window.URL.revokeObjectURL(url);
+            return true;
+        }
+        setTimeout(pageScroll, 100);
+    }
+
+    pageScroll()
+})()
