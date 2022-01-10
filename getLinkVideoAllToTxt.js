09e8b83fdede035617d8e4269410f403d7e62ef1
Nguyễn Văn Hải <91068849+nvhaidev@users.noreply.github.com>
2022-01-10 20:25:16 +0700

    fix tiktok  10/01/2022
    fix tiktok  10/01/2022

--- getLinkVideoAllToTxt.js
+++ getLinkVideoAllToTxt.js
@@ -1,29 +1,36 @@
-javascript: (function () {
-    var video = 0;
+javascript:(() => {
+    function findLink() {
+        const link = [];
+        document.querySelectorAll('div').forEach(((value, key) => {
+            if (document.querySelectorAll('div')[key].className.includes('DivPlayerContainer')) {
+              link.push(document.querySelectorAll('div')[key][Object.keys(document.querySelectorAll('div')[key])[0]].child.memoizedProps.playUrl)
+            }
+        }))
+        return link
+    }
+    function downloadLink(link) {
+        const uid = window.location.href.split('/')[3].split('?')[0];
+        const file = new Blob([JSON.stringify(link)], {type: 'text/plain'});
+        const url = window.URL.createObjectURL(file);
+        const b = document.createElement("a");
+        b.style.display = "none";
+        b.href = url;
+        b.setAttribute("download", `${uid}-Namdeptrai.txt`);
+        b.download = `${uid}-Namdeptrai.txt`;
+        document.body.appendChild(b);
+        b.click();
+        window.URL.revokeObjectURL(url);
+    }
+    var i = 0;
 
     function pageScroll() {
+        i+=2;
+        const canvas = document.querySelectorAll('canvas').length;
         window.scrollBy(0, 1000);
-        let max = document.querySelectorAll('a.video-feed-item-wrapper').length;
-        video += 2;
-        if (video > max) {
-            const myNodelist = document.querySelectorAll("a.video-feed-item-wrapper");
-            const uid = window.location.href.split('/')[3].split('?')[0];
-            const link = [];
-            for (let i = 0; i < myNodelist.length; i++) {
-                link.push(document.querySelectorAll("a.video-feed-item-wrapper")[i][Object.keys(document.querySelectorAll('a.video-feed-item-wrapper')[i])[1]].children.props.videoData.video.playAddr)
-            }
-            const file = new Blob([JSON.stringify(link)], {type: 'text/plain'});
-            const url = window.URL.createObjectURL(file);
-            const b = document.createElement("a");
-            b.style.display = "none";
-            b.href = url;
-
-            b.setAttribute("download", `${uid}-Namdeptrai.txt`);
-            b.download = `${uid}-Namdeptrai.txt`;
-            document.body.appendChild(b);
-            b.click();
-            window.URL.revokeObjectURL(url);
-            return true;
+        if (i > canvas) {
+            const link = findLink();
+            downloadLink(link);
+            return;
         }
         setTimeout(pageScroll, 100);
     }
